81e310b795aefa6d701b453620e318c423882c85
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-01-06 10:52:57 +0600

    create Auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const register = async(req, res) => {
+    try {
+        const { name, email, password, gander } = req.body;
+    }catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+};
+export const login = (req, res) => {
+    res.send('Login');
+};
+export const logout = (req, res) => {
+    res.send('Logout successfully');
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
 import express from 'express';
 import dotenv from 'dotenv';
+import authRouters from './routes/auth.routes.js';
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+
 app.get('/', (req, res) => {
     res.send(`Server is running on port ${PORT}`);
 })
+
+app.use('/api/auth', authRouters);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
