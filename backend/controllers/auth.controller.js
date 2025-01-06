a4669db3f5dc6390520ffb931abd103a01c4d538
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-01-06 17:45:39 +0600

    make a custom authentication system

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,61 @@
-export const register = async(req, res) => {
-    try {
-        const { name, email, password, gander, profilePicture } = req.body;
-    }catch (error) {
-        res.status(500).send({ message: error.message });
+import User from "../models/user.model.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, username, password, confirmPassword, gander, profilePicture } =
+      req.body;
+
+      if (password !== confirmPassword) {
+        return res.status(400).json({ error: "Passwords don't match" });
     }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+        return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // https://avatar-placeholder.iran.liara.run/document
+
+    const boyProfilePicture = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePicture = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password,
+      gander,
+      profilePicture: gander === "male" ? boyProfilePicture : girlProfilePicture,
+
+        })
+
+    await newUser.save();
+    res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        gander: newUser.gander,
+        profilePicture: newUser.profilePicture,
+        
+    });
+
+  } catch (error) {
+    console.log(
+        `Error in register controller: ${error.message}`
+    );
+    res.status(500).json({error: "Internal server error"});
+  }
 };
+
+
+
 export const login = (req, res) => {
-    res.send('Login');
+  res.send("Login");
 };
+
+
+
 export const logout = (req, res) => {
-    res.send('Logout successfully');
+  res.send("Logout successfully");
 };
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
+app.use(express.json());
 
 app.use('/api/auth', authRouters);
 
