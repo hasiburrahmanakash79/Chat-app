d656adfc90f7a9d9d77c9b708e117d71a304e93e
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-01-10 12:07:02 +0600

    make a custom authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,51 +4,51 @@ export const register = async (req, res) => {
 
   res.send("Register");
 
-  // try {
-  //   const { name, email, username, password, confirmPassword, gander, profilePic } =
-  //     req.body;
+  try {
+    const { name, email, username, password, confirmPassword, gander, profilePic } =
+      req.body;
 
-  //     if (password !== confirmPassword) {
-  //       return res.status(400).json({ error: "Passwords don't match" });
-  //   }
+      if (password !== confirmPassword) {
+        return res.status(400).json({ error: "Passwords don't match" });
+    }
 
-  //   const user = await User.findOne({ username });
+    const user = await User.findOne({ username });
 
-  //   if (user) {
-  //       return res.status(400).json({ error: "Username already exists" });
-  //   }
+    if (user) {
+        return res.status(400).json({ error: "Username already exists" });
+    }
 
-  //   // https://avatar-placeholder.iran.liara.run/document
+    // https://avatar-placeholder.iran.liara.run/document
 
-  //   const boyProfilePicture = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-  //   const girlProfilePicture = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const boyProfilePicture = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePicture = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-  //   const newUser = new User({
-  //     name,
-  //     email,
-  //     username,
-  //     password,
-  //     gander,
-  //     profilePic: gander === "male" ? boyProfilePicture : girlProfilePicture,
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password,
+      gander,
+      profilePic: gander === "male" ? boyProfilePicture : girlProfilePicture,
 
-  //       })
+        })
 
-  //   await newUser.save();
-  //   res.status(201).json({
-  //       _id: newUser._id,
-  //       name: newUser.name,
-  //       email: newUser.email,
-  //       gander: newUser.gander,
-  //       profilePicture: newUser.profilePicture,
+    await newUser.save();
+    res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        gander: newUser.gander,
+        profilePicture: newUser.profilePicture,
         
-  //   });
+    });
 
-  // } catch (error) {
-  //   console.log(
-  //       `Error in register controller: ${error.message}`
-  //   );
-  //   res.status(500).json({error: "Internal server error"});
-  // }
+  } catch (error) {
+    console.log(
+        `Error in register controller: ${error.message}`
+    );
+    res.status(500).json({error: "Internal server error"});
+  }
 };
 
 
